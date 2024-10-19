827b8a7f10e69e743a5aec5dc7446a98d46b6a5f
Katsumi404 <88777404+Katsumi404@users.noreply.github.com>
2024-10-19 12:48:10 +0100

    Added labels for the debugging system

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Speed of rotation
-    public float rotationSpeed = 100f;
-
     // Update is called once per frame
     void Update()
     {
-        // Rotate the object around its up axis at the specified speed
-        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        transform.Rotate (new Vector3(15,30,45) * Time.deltaTime);
     }
 }
